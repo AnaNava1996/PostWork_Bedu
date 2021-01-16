239a1648d647630afc325c735540012e1a40ef56
AnaNava1996 <ananava1996@gmail.com>
2021-01-15 19:34:26 -0600

    postwork 1 no esta listo aun

--- Postwork_1/PostWork_1.R
+++ Postwork_1/PostWork_1.R
@@ -1,5 +1,26 @@
+library(dplyr)
+library(ggplot2)
+
+
+# 1- Cargamos el csv del enlace en la variable primera19_20
+
 primera19_20 <- read.csv("https://www.football-data.co.uk/mmz4281/1920/SP1.csv")
+
 head(primera19_20)
+dim(primera19_20)
+class(primera19_20)
+str(primera19_20)
+
+# 2- Del data frame que resulta de importar los datos a R, extrae las columnas que contienen los números de 
+# goles anotados por los equipos que jugaron en casa (FTHG) y los goles anotados por los equipos que jugaron como visitante (FTAG)
+
+goles <- select(primera19_20, FTHG, FTAG)
+goles <- rename(goles, casa=FTHG, visitante=FTAG )
+
+# 3- Consulta cómo funciona la función table en R al ejecutar en la consola ?table
+
+?table
+
 
 
 
