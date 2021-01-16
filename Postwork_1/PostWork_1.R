7ead5ab4d06982525339a4711de1879cec76ae74
AnaNava1996 <ananava1996@gmail.com>
2021-01-15 20:16:22 -0600

    postwork 1 no esta listo aun

--- Postwork_1/PostWork_1.R
+++ Postwork_1/PostWork_1.R
@@ -14,13 +14,16 @@ str(primera19_20)
 # 2- Del data frame que resulta de importar los datos a R, extrae las columnas que contienen los números de 
 # goles anotados por los equipos que jugaron en casa (FTHG) y los goles anotados por los equipos que jugaron como visitante (FTAG)
 
-goles <- select(primera19_20, FTHG, FTAG)
+goles <- select(primera19_20, HomeTeam, AwayTeam, FTHG, FTAG)
 goles <- rename(goles, casa=FTHG, visitante=FTAG )
-
+goles
 # 3- Consulta cómo funciona la función table en R al ejecutar en la consola ?table
 
 ?table
 
-
+#  Posteriormente elabora tablas de frecuencias relativas para estimar las siguientes probabilidades:
+#  La probabilidad (marginal) de que el equipo que juega en casa anote x goles (x = 0, 1, 2, ...)
+#  La probabilidad (marginal) de que el equipo que juega como visitante anote y goles (y = 0, 1, 2, ...)
+#  La probabilidad (conjunta) de que el equipo que juega en casa anote x goles y el equipo que juega como visitante anote y goles (x = 0, 1, 2, ..., y = 0, 1, 2, ...)
 
 
